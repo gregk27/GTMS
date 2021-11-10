c6c7c55bd87655e89044c26b73ca8d23bb7ebfaf
gregk27 <git@gregk.ca>
2021-11-10 09:27:38 -0500

    Cache audio on frontend to improve response time

--- static/audio.js
+++ static/audio.js
@@ -3,31 +3,49 @@
 
 const socket = io();
 /** @type HTMLAudioElement */
-var audioPlayer;
+var audioPlayer = null;
+/** @type Object.<string, HTMLAudioElement> */
+var players = {};
 var busy = false;
 
 var queue = []
 
-window.onload = () => {
-    audioPlayer = document.createElement("audio");
-    document.body.appendChild(audioPlayer);
-    audioPlayer.addEventListener("ended", ()=>{
-        if(queue.length > 0){
-            busy = true;
-            audioPlayer.src = queue.shift();
-            audioPlayer.play();
-        } else {
-            busy = false;
-        }
-    })
+function cacheAudio(src){
+    if(players[src] != undefined && players[src] != null) return;
+    players[src] = document.createElement("audio");
+    document.getElementById("audios").appendChild(players[src]);
+    players[src].addEventListener("ended", playNextAudio);
+
+    players[src].src = src;
+}
+
+function playNextAudio(){
+    console.log("Playing next" + queue)
+    if(queue.length > 0){
+        busy = true;
+        audioPlayer = players[queue.shift()];
+        audioPlayer.play();
+    } else {
+        busy = false;
+    }
+}
+
+window.onload = async () => {
+    // Create an invisible div to hold the audio elements
+    let div = document.createElement("div");
+    div.id = "audios";
+    div.style.display = 'none';
+    document.body.appendChild(div);
+
+    config = await(await(fetch("/config/audio"))).json();
+    for(let a of config.sequence)
+        cacheAudio(a.source);
 }
 
 socket.on("queueAudio", (src) => {
+    console.log("QUEUEING "+src);
+    queue.push(src);
     if(!busy){
-        audioPlayer.src = src;
-        audioPlayer.play();
-        busy = true;
-    } else {
-        queue.push(src);
+        playNextAudio();
     }
 })
