aff81c336cfe8aeb53b08089c27578c988b00cd9
gregk27 <git@gregk.ca>
2021-11-10 20:23:16 -0500

    Convert game page to use socket events

--- static/game.js
+++ static/game.js
@@ -1,3 +1,9 @@
+import {socket, init} from '/socketbase.js';
+
+init(["matchLoaded", "matchStarted", "scoreChanged", "matchFinished"]);
+
+var currentMatch = null;
+
 function toMMSS (unix) {
     if(unix < 0) return "00:00";
     unix = Math.floor(unix/1000);
@@ -9,9 +15,8 @@ function toMMSS (unix) {
     return minutes+':'+seconds;
 }
 
-async function update(){
-    let data = await (await fetch("/game/data")).json();
-    console.log(data);
+async function update(data){
+    currentMatch = data;
     document.querySelector("#matchNum").innerText = data.name;
     document.querySelector("#red .teamNum").innerText = data.red.num;
     document.querySelector("#red .teamName").innerText = data.red.name;
@@ -19,12 +24,33 @@ async function update(){
     document.querySelector("#blue .teamNum").innerText = data.blue.num;
     document.querySelector("#blue .teamName").innerText = data.blue.name;
     document.querySelector("#blue .score").innerText = data.blue.score;
-    document.querySelector("#time").innerText = toMMSS(data.endTime - Date.now())
 }
 
-window.onload = ()=>{
-    setInterval(() => {
-        update();
-    }, 500);
-    update();
+window.onload = async ()=>{
+    document.querySelector("#time").innerText = toMMSS(0);
+    update(await (await fetch("/game/data")).json());
+    // If it's reloaded mid-match, use higher frequency since it'll be offset from what it should be
+    setInterval(() => document.querySelector("#time").innerText = toMMSS(currentMatch.endTime - Date.now()), 100);
 };
+
+var timer = null;
+
+socket.on("matchLoaded", (match)=>{
+    update(match);
+    document.querySelector("#time").innerText = toMMSS(match.duration)
+})
+
+socket.on("matchStarted", (match)=>{
+    update(match);
+    // Since this runs on the start event, time should be within 100ms of correct at a constant interval
+    setInterval(() => document.querySelector("#time").innerText = toMMSS(currentMatch.endTime - Date.now()), 1000);
+})
+
+socket.on("scoreChanged", (match)=>{
+    update(match);
+})
+
+socket.on("matchFinished", (match)=>{
+    if(timer != null) clearInterval(timer);
+    document.querySelector("#time").innerText = toMMSS(0);
+})
