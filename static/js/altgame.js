2cf7071e4aaf40f593b8f369bcdfbb2381153705
gregk27 <git@gregk.ca>
2021-11-16 11:47:44 -0500

    Create alternate game page derived from postgame page

--- static/js/altgame.js
+++ static/js/altgame.js
@@ -1,21 +1,9 @@
-import {socket, init} from '/js/socketbase.js'
-
-init(["matchSaved"])
+import init from "/js/gamebase.js"
 
 var config;
 
-socket.on("matchSaved", (currentMatch)=>{
-    let delta = currentMatch.red.score - currentMatch.blue.score;
-    if(delta > 0) {
-        document.body.style.background = "var(--red-primary)";
-        document.getElementById("banner").innerHTML = `<div>${currentMatch.name}</div><h2>Red Wins!</h2>`;
-    } else if (delta < 0) {
-        document.body.style.background = "var(--blue-primary)";
-        document.getElementById("banner").innerHTML = `<div>${currentMatch.name}</div><h2>Blue Wins!</h2>`;
-    } else {
-        document.body.style.background = "";
-        document.getElementById("banner").innerHTML = `<div>${currentMatch.name}</div><h2>Tie!</h2>`;
-    }
+function update(currentMatch){
+    document.querySelector("#banner div").innerText = currentMatch.name;
 
     document.querySelector(`.alliance#red .score`).innerText = currentMatch.red.score
     document.querySelector(`.alliance#red .team`).innerHTML = `<span>${currentMatch.red.num}</span>${currentMatch.red.name}`
@@ -32,8 +20,10 @@ socket.on("matchSaved", (currentMatch)=>{
         html += `<tr><td>${eval(b.func)(currentMatch.blue)}</td><td>-</td><td>${b.name}</td></tr>`
     }
     document.querySelector('.alliance#blue .breakdown').innerHTML = html;
-});
+}
 
 (async () => {
     config = await (await fetch("/config/postgame")).json()
+    // Load config before initializing
+    init(update, document.querySelector("#banner h2"));
 })();
