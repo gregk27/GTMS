de3332d0a045ee5db18742b0f2a826757dd410e8
gregk27 <git@gregk.ca>
2021-11-17 10:18:00 -0500

    Improve autopage cycling

--- static/js/autopage.js
+++ static/js/autopage.js
@@ -27,14 +27,49 @@ window.onload = () =>{
     window.views = views;
 }
 
+var cycleTimeout = null;
+var scoreboardDuration = 0;
+var scheduleDuration = 0;
+
+(async () => {
+    let cfg = await(await fetch("/config/scoreboard")).json();
+    scoreboardDuration = cfg.duration * cfg.data.length * 1000;
+    scheduleDuration = cfg.duration * 2 * 1000;
+})();
+
+window.cycleScoreboard = cycleScoreboard;
+function cycleScoreboard(){
+    showView(scoreboard);
+    clearTimeout(cycleTimeout);
+    console.log(scoreboardDuration);
+    setTimeout(cycleSchedule, scoreboardDuration);
+}
+
+function cycleSchedule(){
+    showView(schedule);
+    clearTimeout(cycleTimeout);
+    setTimeout(cycleScoreboard, scheduleDuration);
+}
+
 socket.on("matchStarted", ()=>{
     showView(game);
+    clearTimeout(cycleTimeout);
 })
 
 socket.on("matchSaved", ()=>{
     showView(postgame)
+    clearTimeout(cycleTimeout);
+    // Cycle to scoreboard page when done
+    cycleTimeout = setTimeout(()=>{
+        scoreboard.contentWindow.location.reload();
+        // Stagger to leave time for reload
+        cycleTimeout = setTimeout(()=>{
+            cycleScoreboard();
+        }, 100)
+    }, 60000)
 })
 
 socket.on("matchLoaded", ()=>{
-    showView(scoreboard);
+    showView(game);
+    clearTimeout(cycleTimeout);
 })
