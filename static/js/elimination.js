030445ca7346a9af52a154bb1082a44af4bb49b0
gregk27 <git@gregk.ca>
2022-12-03 23:51:49 -0500

    Add custom data to bracket html to determine upcoming match from available information

--- static/js/elimination.js
+++ static/js/elimination.js
@@ -14,6 +14,57 @@ init(["matchFinished"], ()=>{
 // Chain socket events to get elim scores and ranking scoreboard
 socket.on("getEliminationScores", (scores) => {
     results = scores;
+    // results = [
+    //     {
+    //         "number": 1,
+    //         "redTeam": 4000,
+    //         "blueTeam": 5000,
+    //         "redScore": 10,
+    //         "blueScore": 20
+    //     },
+    //     {
+    //         "number": 2,
+    //         "redTeam": 2000,
+    //         "blueTeam": 3000,
+    //         "redScore": 30,
+    //         "blueScore": 20
+    //     },
+    //     {
+    //         "number": 3,
+    //         "redTeam": 1000,
+    //         "blueTeam": 5000,
+    //         "redScore": 30,
+    //         "blueScore": 20
+    //     },
+    //     {
+    //         "number": 4,
+    //         "redTeam": 3000,
+    //         "blueTeam": 4000,
+    //         "redScore": 20,
+    //         "blueScore": 10
+    //     },
+    //     {
+    //         "number": 5,
+    //         "redTeam": 1000,
+    //         "blueTeam": 2000,
+    //         "redScore": 0,
+    //         "blueScore": 8
+    //     },
+    //     {
+    //         "number": 6,
+    //         "redTeam": 5000,
+    //         "blueTeam": 3000,
+    //         "redScore": 0,
+    //         "blueScore": 8
+    //     },
+    //     {
+    //         "number": 7,
+    //         "redTeam": 1000,
+    //         "blueTeam": 3000,
+    //         "redScore": 0,
+    //         "blueScore": 8
+    //     }
+    // ];
     socket.emit("getScoreboard");
 })
 socket.on("getScoreboard", (scoreboard) => {
@@ -55,7 +106,22 @@ function updateBracket() {
         if(loserDiv != null)
             loserDiv.querySelector("p").innerText = `Looser ${i+1}`;
     }
-    document.getElementById(`match-${results.length+1}`)?.classList.add("next");
+
+    var currNum = results.length + 1;
+    var currMatch = document.getElementById(`match-${currNum}`);
+    if(currMatch != null){
+        currMatch?.classList.add("next");
+
+        // Sketchy way to get data about upcoming match
+        document.getElementById("nextRed").innerText = document.getElementById(currMatch.getAttribute("data-red")).innerText;
+        document.getElementById("nextBlue").innerText = document.getElementById(currMatch.getAttribute("data-blue")).innerText;
+        document.getElementById("nextName").innerText = currMatch.innerText;
+    }
+    else {
+        document.getElementById("nextRed").innerText = "???";
+        document.getElementById("nextBlue").innerText = "???";
+        document.getElementById("nextName").innerText = "Match ?";
+    }
 }
 
 
