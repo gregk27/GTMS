bfa4074ea51c5feac1ab88feafaab4c84c95092e
Gregory Kelly <git@gregk.ca>
2022-12-10 10:24:05 -0500

    Create match schedule

--- setup.sql
+++ setup.sql
@@ -3,12 +3,12 @@ DELETE FROM schedule WHERE 1=1;
 DELETE FROM teams WHERE 1=1;
 
 INSERT INTO teams (number, name) VALUES
-	(1, 'VelociKLAPPtors'),
-	(2, 'Sharpshot Atlas'),
-	(3, 'The Best Team'),
+	(1111, 'VelociKLAPPtors'),
+	(2222, 'Sharpshot Atlas'),
+	(3333, 'The Best Team'),
 	(777, 'The Gamblers'),
 	(5272, 'Straight Shooters'),
-	(4, 'Sydenham')
+	(4444, 'Sydenham');
 
 -- Match type is one of PRACTICE, RANKING, ELIMINATION, OTHER
 -- - Practice matches show on schedule but don't get counted for ranking
@@ -16,15 +16,52 @@ INSERT INTO teams (number, name) VALUES
 -- - ELIMINATION matches are same as OTHER, but reserved for future use
 -- - OTHER matches do not appear on schedule or count for ranking
 INSERT INTO schedule (type, prettyName, number, redTeam, blueTeam) VALUES
-	('RANKED', 'Match', 1, 1, 2),
-	('RANKED', 'Match', 2, 3, 1),
-	('RANKED', 'Match', 3, 2, 6),
-	('RANKED', 'Match', 4, 7, 8),
-	('RANKED', 'Match', 5, 9, 1),
-	('RANKED', 'Match', 6, 2, 3),
-	('RANKED', 'Match', 7, 1, 2),
-	('RANKED', 'Match', 8, 6, 7),
-	('RANKED', 'Match', 9, 8, 2);
+	('PRACTICE', 'Practice', 1, 1111, 2222),
+	('PRACTICE', 'Practice', 2, 5272, 3333),
+	('PRACTICE', 'Practice', 3, 4444, 777),
+
+	('RANKED', 'Match', 1, 1111, 3333),
+	('RANKED', 'Match', 2, 4444, 5272),
+	('RANKED', 'Match', 3, 777, 2222),
+
+	('RANKED', 'Match', 4, 3333, 4444),
+	('RANKED', 'Match', 5, 1111, 2222),
+	('RANKED', 'Match', 6, 5272, 777),
+
+	('RANKED', 'Match', 7, 3333, 2222),
+	('RANKED', 'Match', 8, 4444, 777),
+	('RANKED', 'Match', 9, 1111, 5272),
+
+	('RANKED', 'Match', 10, 2222, 4444),
+	('RANKED', 'Match', 11, 1111, 777),
+	('RANKED', 'Match', 12, 5272, 3333),
+
+	('RANKED', 'Match', 13, 1111, 4444),
+	('RANKED', 'Match', 14, 2222, 5272),
+	('RANKED', 'Match', 15, 777, 3333),
+
+	('RANKED', 'Match', 16, 4444, 5272),
+	('RANKED', 'Match', 17, 1111, 3333),
+	('RANKED', 'Match', 18, 777, 2222),
+
+	('RANKED', 'Match', 19, 3333, 4444),
+	('RANKED', 'Match', 20, 5272, 777),
+	('RANKED', 'Match', 21, 1111, 2222),
+
+	('RANKED', 'Match', 22, 4444, 777),
+	('RANKED', 'Match', 23, 3333, 2222),
+	('RANKED', 'Match', 24, 1111, 5272),
+
+	('RANKED', 'Match', 25, 2222, 4444),
+	('RANKED', 'Match', 26, 1111, 777),
+	('RANKED', 'Match', 27, 5272, 3333),
+
+	('RANKED', 'Match', 28, 1111, 3),
+	('RANKED', 'Match', 29, 2222, 5272),
+	('RANKED', 'Match', 30, 777, 6);
+
+-- Exhibition match inserted at end
+	('OTHER', 'Alliances Demo', -1, 9999, 9999)
 
 SELECT * FROM teams;
 SELECT * FROM schedule;
